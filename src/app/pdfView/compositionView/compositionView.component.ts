d30cfe6693cb22eee657d99e21d99a8053e6ea79
Julien Ferezou <julien.ferezou@soprasteria.com>
2017-09-07 18:28:47 +0200

    maj

--- src/app/pdfView/compositionView/compositionView.component.ts
+++ src/app/pdfView/compositionView/compositionView.component.ts
@@ -1,12 +1,29 @@
 
 import { Component, Input } from '@angular/core';
+import { FormGroup, FormBuilder, NgForm, Validators } from '@angular/forms';
+import {OnInit} from '@angular/core';
  
 @Component({
-  selector: 'compositionview',
+  selector: '[compositionview]',
   templateUrl: './compositionView.component.html',
   styleUrls: ['./compositionView.component.css']
 })
-export class CompositionViewComponent{
-  @Input() composition: string;
-  
+export class CompositionViewComponent implements OnInit{
+  @Input() composition;
+  dominantIsChecked = false;
+  accompagnementIsChecked = false;
+  isoleIsChecked = false;
+  isvalid=false;
+  ngOnInit(): void {
+    if(this.composition.type === 'Dominant') {
+		this.dominantIsChecked=true;
+	}
+	else 
+    if(this.composition.type === 'Accompagnement') {
+		this.accompagnementIsChecked=true;
+	}
+	else {
+		this.isoleIsChecked=true;
+	}
+  }
 }
--- src/app/pdfView/pdfView.component.ts
+++ src/app/pdfView/pdfView.component.ts
@@ -9,9 +9,7 @@ import { Component, Input, OnChanges, SimpleChanges } from '@angular/core';
 export class PdfViewComponent implements OnChanges {
   @Input() currentItem: string;
   
-   refresh(newCurrentItem){
-    this.currentItem = newCurrentItem;
-  }    
+  
   ngOnChanges(changes: SimpleChanges) {
         console.log('onChange fired');
 		this.currentItem=changes.currentItem.currentValue;
