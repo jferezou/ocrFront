47051207f29d829d2f2c87af0b6d7bcab8bc3ba0
Julien Ferezou <julien.ferezou@soprasteria.com>
2017-09-08 16:22:03 +0200

    maj pour post

--- src/app/pdfView/pdfView.component.ts
+++ src/app/pdfView/pdfView.component.ts
@@ -1,6 +1,7 @@
 
 import { Component, Input, ViewChildren, OnChanges, SimpleChanges } from '@angular/core';
 import { NgForm } from '@angular/forms';
+import { Http, Response, Headers } from '@angular/http';
  
 @Component({
   selector: 'mypdfview',
@@ -8,14 +9,22 @@ import { NgForm } from '@angular/forms';
   styleUrls: ['./pdfview.component.css']
 })
 export class PdfViewComponent implements OnChanges {
-  @Input() currentItem: string;
+	@Input() currentItem: string;
   
+	private saveUrl ="http://localhost:8089/ocr/services/rest/traitement/save";
+  constructor(private http: Http) {}
 
   ngOnChanges(changes: SimpleChanges) {
         console.log('onChange fired');
 		this.currentItem=changes.currentItem.currentValue;
     }
+		
+		
 	public onSubmit(f: NgForm) {
-		console.log(f.value); 
+		const body = JSON.stringify(f.value); 
+		var headers = new Headers();
+		headers.append('Content-Type', 'application/json');
+		headers.append('Access-Control-Allow-Origin', '*');
+		this.http.post(this.saveUrl, body,{headers: headers}).subscribe(r=>{});;
 	}
 }
